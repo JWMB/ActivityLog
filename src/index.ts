7d14c31eeded415232ef4486dfb1aadc2b7f87fe
Jonas Beckeman <jonas.beckeman@gmail.com>
2024-06-11 20:39:01 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Demo function for template repository
+ *
+ * @remarks
+ * Read more about TSDoc at: {@link https://github.com/microsoft/tsdoc}
+ *
+ * @returns Nothing!
+ *
+ * @beta
+ */
+export function demo(): string {
+  return "hello world!"
+}
+demo()
